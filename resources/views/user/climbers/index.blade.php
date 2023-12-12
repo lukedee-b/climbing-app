624f6807940f44690787ac93e8b0cf424de54d99
lukedee-b <n00220386@iadt.ie>
2023-12-12 13:44:33 +0000

    fixed create function for all tables

--- resources/views/user/climbers/index.blade.php
+++ resources/views/user/climbers/index.blade.php
@@ -1,20 +1,15 @@
-@extends('layouts.admin')
-
-@section('header')
-<h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-    {{ __('Climbers') }}
-</h2>
-@endsection
-
-
-@section('content')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Climbers
+        </h2>
+    </x-slot>
 
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900 dark:text-gray-100">
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <button type="submit" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"><a href="{{ route('admin.climbers.create') }}">Create</a></button>
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
@@ -61,4 +56,4 @@
         </div>
     </div>
 </div>
-@endsection
+</x-app-layout>
